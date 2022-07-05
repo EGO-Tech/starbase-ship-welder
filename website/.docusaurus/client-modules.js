e4dd4a3bdc96bb831df66ca3b1d4efd9c37be90f
Vincent Teo <vinteo@gmail.com>
2022-07-05 12:00:34 +1000

    build: docusaurus

---
+++ website/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/home/vincent/workspace/starbase-ship-welder/website/node_modules/infima/dist/css/default/default.css'),
+  require('/home/vincent/workspace/starbase-ship-welder/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/vincent/workspace/starbase-ship-welder/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/vincent/workspace/starbase-ship-welder/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/vincent/workspace/starbase-ship-welder/website/src/css/custom.css'),
+];
